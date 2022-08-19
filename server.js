43ec9bb47292f127e0eb9eeca8c8aa6ec62b7d7c
Paweł Szczygieł <pawel86szczygiel@gmail.com>
2022-08-19 17:03:21 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const articleRouter = require('./routes/articles');
+const app = express();
+
+mongoose.connect('mongodb://localhost/blog', {
+    useNewUrlParser: true, useUnifiedTopology: true });
+
+app.set('view engine', 'ejs');
+
+app.use('/articles', articleRouter);
+
+app.get('/', (req, res) => {
+
+    const articles = [{
+        title: 'Test Article',
+        createdAt: new Date(),
+        description: 'Test description'
+    }]
+
+    res.render('articles/index', {articles})
+});
+
+app.listen(5000, console.log('app listen on port 5000'));
